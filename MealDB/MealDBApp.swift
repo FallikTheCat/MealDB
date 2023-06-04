2b6aeea55e26388b9b552ef7683d7e815a9dbd54
FallikTheCat <baturaykocc@hotmail.com>
2023-06-04 16:43:11 -0500

    Initial Commit

---
+++ MealDB/MealDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealDBApp.swift
+//  MealDB
+//
+//  Created by Baturay Koç on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct MealDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
